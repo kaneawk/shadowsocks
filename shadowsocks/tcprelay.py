f30cc09e6d9fc3688c7933f921d4ae68c846cd98
BreakWa11 <mmgac001@gmail.com>
2016-07-19 16:41:42 +0800

    check decode remote_addr

--- shadowsocks/tcprelay.py
+++ shadowsocks/tcprelay.py
@@ -354,7 +354,7 @@ class TCPRelayHandler(object):
         if type(host_list) == list:
             host_post = common.to_str(host_list[((hash_code & 0xffffffff) + addr) % len(host_list)])
         else:
-            host_post = host_list
+            host_post = common.to_str(host_list)
         items = host_post.rsplit(':', 1)
         if len(items) > 1:
             try:
@@ -464,20 +464,18 @@ class TCPRelayHandler(object):
                 if data is None:
                     data = self._handel_protocol_error(self._client_address, ogn_data)
                 header_result = parse_header(data)
+                if header_result is not None:
+                    try:
+                        common.to_str(remote_addr)
+                    except Exception as e:
+                        header_result = None
                 if header_result is None:
                     data = self._handel_protocol_error(self._client_address, ogn_data)
                     header_result = parse_header(data)
             connecttype, remote_addr, remote_port, header_length = header_result
-            try:
-                common.connect_log("%s connecting %s:%d from %s:%d via port %d" %
+            common.connect_log("%s connecting %s:%d from %s:%d via port %d" %
                         ((connecttype == 0) and 'TCP' or 'UDP',
                             common.to_str(remote_addr), remote_port,
-                            self._client_address[0], self._client_address[1],
-                            self._config['server_port']))
-            except Exception as e:
-                common.connect_log("%s connecting %s:%d from %s:%d via port %d" %
-                        ((connecttype == 0) and 'TCP' or 'UDP',
-                            binascii.hexlify(remote_addr), remote_port,
                             self._client_address[0], self._client_address[1],
                             self._config['server_port']))
             self._remote_address = (common.to_str(remote_addr), remote_port)
